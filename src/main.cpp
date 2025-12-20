02dd4edc96c8833d8eefd3597539c1b1ff2e06aa
Barna Temesi <barna.temesi@gmail.com>
2025-12-20 17:12:18 +0100

    slight rework of main

--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,17 @@ using namespace std;
 
 int main()
 {
-    cout<<"Welcome to Online IDE!! Happy Coding :)" << endl;
-
     Eigen::MatrixXd m(2, 2);
     m(0, 0) = 3;
     m(1, 0) = 2.5;
     m(0, 1) = -1;
     m(1, 1) = m(1, 0) + m(0, 1);
     cout << m << endl;
+
+    // body of algorithm
+    NeuralNetwork n({ 2, 3, 1 });
+    std::vector<RowVector*> in_dat;
+    std::vector<RowVector*> out_dat;
     
     return 0;
 }
