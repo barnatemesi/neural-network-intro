4c88d13fa1a218c2dd087769b9947b3a545f29e5
Barna Temesi <barna.temesi@gmail.com>
2025-12-20 21:42:55 +0100

    cleanup

--- inc/neural_network.h
+++ inc/neural_network.h
@@ -20,10 +20,10 @@ typedef Eigen::VectorXf ColVector;
 // neural network implementation class!
 class NeuralNetwork {
 private:
-    std::vector<uint> topology; // topology
+    vector<uint> topology; // topology
 public:
     // constructor
-    NeuralNetwork(std::vector<uint> topology, Scalar learningRate = Scalar(0.005));
+    NeuralNetwork(vector<uint> topology, Scalar learningRate = Scalar(0.005));
 
     // function for forward propagation of data
     void propagateForward(RowVector& input);
@@ -37,6 +37,8 @@ public:
     // function to update the weights of connections
     void updateWeights();
 
+    void printWeights();
+
     // function to train the neural network give an array of data points
     vector<Scalar> train(vector<RowVector*> input_data, vector<RowVector*> output_data);
 
@@ -47,10 +49,10 @@ public:
           it also makes our neural network class less heavy!! It would be nice if you can use
           smart pointers instead of usual ones like this
         */
-    std::vector<RowVector*> neuronLayers; // stores the different layers of out network
-    std::vector<RowVector*> cacheLayers; // stores the unactivated (activation fn not yet applied) values of layers
-    std::vector<RowVector*> deltas; // stores the error contribution of each neurons
-    std::vector<Matrix*> weights; // the connection weights itself
+    vector<RowVector*> neuronLayers; // stores the different layers of out network
+    vector<RowVector*> cacheLayers; // stores the unactivated (activation fn not yet applied) values of layers
+    vector<RowVector*> deltas; // stores the error contribution of each neurons
+    vector<Matrix*> weights; // the connection weights itself
     Scalar learningRate;
 
     // destructor -> free neuronLayers, cacheLayers, weights, and deltas
